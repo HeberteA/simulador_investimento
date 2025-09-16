70005a8ca4f9ba7e4776552aea366809a7f62fd7
HeberteA <heberte.2307@gmail.com>
2025-09-16 08:56:23 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -174,5 +174,8 @@ def generate_pdf(data):
         pdf.cell(col_widths[3], 6, format_currency(int_val), 1, 0, 'R')
         pdf.cell(col_widths[4], 6, format_currency(inst_with_int), 1, 1, 'R')
     
-    return pdf.output(dest='S').encode('latin-1')
+    output = pdf.output(dest='S')
+    if isinstance(output, str):
+        return output.encode('latin-1')
+    return output
   
