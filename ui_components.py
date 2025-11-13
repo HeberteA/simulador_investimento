c8edd54727da51c399cdac17d1f524bc6001215a
HeberteA <heberte.2307@gmail.com>
2025-11-13 10:08:27 -0300

    Add additional markdown sections in ui_components.py

--- ui_components.py
+++ ui_components.py
@@ -59,6 +59,10 @@ def display_full_results(results, show_save_button=False, show_download_button=F
             st.markdown("")
             st.markdown("")
             st.markdown("")
+            st.markdown("")
+            st.markdown("")
+            st.markdown("")
+            st.markdown("")
             co1, co2, co3 = st.columns([1, 2, 1])
             with co2:
                 resultado_final_str = format_currency(results.get('resultado_final_investidor', 0))
@@ -97,7 +101,6 @@ def display_full_results(results, show_save_button=False, show_download_button=F
             c_juros.metric("Custo do Capital (Juros)", f"+ {format_currency(results.get('juros_investidor', 0))}")
 
             st.metric("Custo Total da Obra", f"={format_currency(results.get('total_construction_cost', 0))}")
-            st.divider()
             st.metric("Resultado Operacional do Projeto", f"={format_currency(results.get('final_operational_result', 0))}")
             st.divider()
             st.markdown("##### Rentabilidade do Investimento")
