bf0f4a2e030097e049949563ba2bb64ea53f0426
HeberteA <heberte.2307@gmail.com>
2025-11-14 11:51:16 -0300

    Update ui_components.py

--- ui_components.py
+++ ui_components.py
@@ -98,7 +98,7 @@ def display_full_results(results, show_save_button=False, show_download_button=F
 
         with colu2: 
             
-            co1, co2, co3 = st.columns([1, 2, 1])
+            co1, co2, co3 = st.columns([1, 5, 1])
             with co2:
                 st.markdown("")
                 st.markdown("")
