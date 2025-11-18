a5575baf66acdd9f0e246c1c47127ea5bc75aa25
HeberteA <heberte.2307@gmail.com>
2025-11-18 09:29:50 -0300

    Update ui_components.py

--- ui_components.py
+++ ui_components.py
@@ -62,7 +62,7 @@ def display_full_results(results, show_save_button=False, show_download_button=F
                 c_troca.metric("Custo Troca de Área", f"+ {format_currency(results.get('area_exchange_value', 0))}")
                 st.metric("Custo Total da Obra", f"= -{format_currency(results.get('total_construction_cost', 0))}")
                     
-        colu1, colu2 = st.columns([1, 1])
+        colu1, colu2 = st.columns([1.25, 1])
         with colu1:
             st.markdown("")
             st.markdown("##### Retorno sobre o Investimento")
