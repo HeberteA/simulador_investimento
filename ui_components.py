3235005d54fdbc2acc95210af4d9853cb930a772
HeberteA <heberte.2307@gmail.com>
2025-11-18 09:12:18 -0300

    Update ui_components.py

--- ui_components.py
+++ ui_components.py
@@ -57,9 +57,9 @@ def display_full_results(results, show_save_button=False, show_download_button=F
         with col2:
             st.markdown("##### Resumo do Projeto Imobiliário")
             st.metric("VGV (Valor Geral de Venda)", format_currency(results.get('vgv', 0)))
-            col5, col6 = st.columns([3, 1])
+            col5, col6 = st.columns([4, 1])
             with col5:
-                with st.container(height=400):
+                with st.container(height=300):
                     st.metric("Custo Físico da Obra", format_currency(results.get('cost_obra_fisica', 0)))
                     c_troca, c_juros = st.columns([2, 2])
                     c_juros.metric("Custo do Capital (Juros)", f"+ {format_currency(results.get('juros_investidor', 0))}")
