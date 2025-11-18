85043160c00720b8da72e99b1a4eebbb4ab52752
HeberteA <heberte.2307@gmail.com>
2025-11-18 09:28:57 -0300

    Update ui_components.py

--- ui_components.py
+++ ui_components.py
@@ -112,7 +112,7 @@ def display_full_results(results, show_save_button=False, show_download_button=F
             <div style="
                 background-color: rgba(0,0,0,0);
                 border-radius: 10px; 
-                padding: 25px; 
+                padding: 20px; 
                 border: 1px solid {THEME_PRIMARY_COLOR};
             ">
                 <p style="
